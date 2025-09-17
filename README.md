2fbe7be7d5b83fac8a504200036ccc4cab2803a9
gonzalezemyl-commits <gonzalez.emyl@gmail.com>
2025-09-16 18:27:59 -0600

    creación de notebook y app

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# sprint_7_project
-Repositorio de mi proyecto del Sprint 7 de Triple Ten
+**Proyecto del Sprint 7 | Emyl Eduardo González Neri**
+
+Este proyecto tendrá como propósito demostrar los conocimientos adquiridos a lo largo del Sprint 7 del bootcamp de Análisis de Datos de TripleTen. Este notebook servirá para una actividad de análisis exploratorio de datos, en el que haré unas gráficas con plotly express, que posteriormente formarán parte de la app que se desplegará como parte del proyecto.
---
+++ app.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import numpy as np
+import plotly.express as px
+import streamlit as st
+
+df = pd.read_csv('vehicles_us.csv')
+hist_button = st.button('Mostrar Histograma')
+scatter_button = st.button('Mostrar Gráfico de Dispersión')
+
+if hist_button:
+
+    st.wrtite('Histograma de Precios de Autos')
+
+    bins = np.arange(1000, 61000, 1000)
+    fig = px.histogram(df,x='price',nbins=500)
+    fig.update_xaxes(range=[1000,61000])
+    
+    st.plotly_chart(fig, use_container_width=True)
+
+if scatter_button:
+
+    st.write('Valor del Vehículo vs Kilometraje')
+
+    fig = px.scatter(df,x='odometer',y='price')
+
+    st.plotly_chart(fig, use_container_width=True)
+
